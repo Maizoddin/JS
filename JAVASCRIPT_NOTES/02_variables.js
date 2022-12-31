235d8e564eb540513433ad04131f14ab58a1e5b5
Maizoddin <95971167+Maizoddin@users.noreply.github.com>
2022-12-31 16:50:22 +0000

    configuration

---
+++ JAVASCRIPT_NOTES/02_variables.js
@@ -0,0 +1,14 @@
+const accountNumber = 432115
+let name = "john"
+console.log(accountNumber,name)
+
+/* Const : It's value is constant
+           It's scope is block
+           we can't change it's value later
+           we can't leave without iniltialising
+           Ex :   const id;    */
+
+/*  let : It's value can be changed
+          it's scope is block
+          we can leave it without initialising
+          Ex : let name;    */
---
+++ JAVASCRIPT_NOTES/03_dataTypes.js
@@ -0,0 +1,36 @@
+// Premetive Data Types
+// NN BB SS U
+// number null boolean BigInt string symbol undefined
+
+let a = 20;             // number
+let b = null;           // null
+let c = true            // boolean
+let d = BigInt(2**100)  // BigInt
+let e = "JavaScript"    //string
+let f = undefined       //undefined
+let g;                  //Default undefined
+
+
+//  Important
+console.log(typeof(b)); // Type of Undefined is Undefined
+console.log(typeof g);  // Type of null is object
+
+// Non Premetive Data Types
+// Object
+let score = {"john":25,"sam":50}
+score["tom"] = 100
+console.log(score["tom"]);
+
+
+// Example 
+const userId = 200;
+let userName = "Maizoddin"
+let isValidUser = true
+let paymentMode;
+// Modern Printing
+console.log(`
+userId : ${userId}
+userName : ${userName}
+isVslidUser : ${isValidUser}
+paymentMode : ${paymentMode}
+`);
