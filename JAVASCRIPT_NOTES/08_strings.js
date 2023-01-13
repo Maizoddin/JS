a9077aaf67815eb9a38e46396dd9405f1b79005b
Maizoddin <95971167+Maizoddin@users.noreply.github.com>
2023-01-13 17:45:15 +0000

    string methods

---
+++ JAVASCRIPT_NOTES/08_strings.js
@@ -0,0 +1,31 @@
+let name = "johnny singh";
+
+// length
+console.log(name.length);     // 12
+
+// toUpperCase
+console.log(name.toUpperCase());   // JOHNNY SINGH
+
+// toLowerCase
+console.log(name.toLowerCase());   // johnny singh
+
+// replace
+console.log(name.replace("johnny", "tommy"));  // tommy singh
+
+// slice
+console.log(name.slice(0, 4));    // john
+
+// trim
+console.log(name.trim());     // remove all extra space present at start and end of string
+
+// indexOf
+console.log(name.indexOf("y"));  // 5
+
+// endsWith
+console.log(name.endsWith("singh"));  // true
+
+// startsWith
+console.log(name.startsWith("johnny")); // true
+
+// includes
+console.log(name.includes("john"));  //true
