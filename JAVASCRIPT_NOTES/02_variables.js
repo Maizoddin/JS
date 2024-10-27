1e1a2984a22988c2c3c46ff69e2f031609ebcbf7
Maizoddin <95971167+Maizoddin@users.noreply.github.com>
2024-10-27 14:26:16 +0530

    Update 02_variables.js

--- JAVASCRIPT_NOTES/02_variables.js
+++ JAVASCRIPT_NOTES/02_variables.js
@@ -12,3 +12,32 @@ console.log(accountNumber,name)
           it's scope is block
           we can leave it without initialising
           Ex : let name;    */
+
+
+// =================================================
+
+// function x(){
+//     for (let i=1; i <= 5; i++){
+//         setTimeout(function(){
+//             console.log(i)
+//         }, i*1000)
+//     }
+// }
+
+// x()
+Output : 1 2 3 4 5
+because let will create new variable for each block since its block scoped
+
+// ===========================================================
+
+// function x(){
+//     for (var i=1; i <= 5; i++){
+//         setTimeout(function(){
+//             console.log(i)
+//         }, i*1000)
+//     }
+// }
+
+// x()
+Output : 6 6 6 6 6
+because only one variable will be created and reference will be shared across all blocks
