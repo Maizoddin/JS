4924ee3d2974b5fcd31156f0b88105a1a10c3e73
Maizoddin <95971167+Maizoddin@users.noreply.github.com>
2024-11-15 11:29:00 +0530

    Update 36_async_await.js

--- JAVASCRIPT_NOTES/36_async_await.js
+++ JAVASCRIPT_NOTES/36_async_await.js
@@ -51,7 +51,47 @@ fnP()
 // Output
 Parent Function
 
-// after 10 sec both promises will get resolved
+// after 10 sec both promises will get resolved because both are outside of the function and independent
 Promise1 resolved
 Child Function
 Promise2 resolved
+
+================================================
+
+Example :
+const cart = ["pants", "shirts"]
+const placeOrder = (cart) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>resolve("123"), 3000)
+    })
+}
+
+const proceedToPayment = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=> resolve("Payment Successful"), 3000)
+    })
+}
+
+const summary = new Promise((resolve, reject) => {
+    setTimeout(()=>resolve("Order Completed"), 3000)
+})
+
+const app = async () => {
+    console.log("Process Started")
+    const orderId = await placeOrder(cart)
+    console.log("Order Id :", orderId)
+    const paymentStatus = await proceedToPayment()
+    console.log("Payment Status :", paymentStatus)
+    const sm = await summary
+    console.log(sm)
+}
+
+app()
+
+// Output :
+Process Started
+// after 3 sec
+Order Id : 123
+// after 3 sec
+Payment Status : Payment Successful
+Order Completed
